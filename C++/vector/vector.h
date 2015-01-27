19290fa3701899b8f709849a11f1a66447add669
Quinn Perfetto <thequinnftw@gmail.com>
2015-01-27 16:52:56 -0500

    added more functions to vector, including assign, constant indexing, etc

--- C++/vector/vector.h
+++ C++/vector/vector.h
@@ -11,6 +11,7 @@ class vector {
         T* data_;
         void resize();
     public:
+        // Constructors and destructor
         vector() : max_size_{10}, size_{0} {
             data_ = new T[max_size_];
         }
@@ -26,15 +27,31 @@ class vector {
             delete[] data_;
         }
         
-
+        // Adding/deleting
         void push_back(T x);
+        template <typename iter>
+        void assign(iter first, iter last);
+        void assign(std::size_t n, const T& v);
 
+        // Iterators
         T* begin() { return data_; }
         T* end() { return data_ + size_; }
-        
+        T& front() { return data_[0]; }
+        T& back() { return data_[size_ - 1]; }
+
+        // Info functions
         std::size_t size() const { return size_; }
- 
+        std::size_t max_size() const { return max_size_; }
+        bool empty() const { return size_ == 0; }
+        
+        // Memory functions
+        void resize(std::size_t n);
+        void shrink_to_fit();
+        void reserve(std::size_t n);
+
+        // Operators
         T& operator[](std::size_t i)  { return data_[i]; }
+        T const& operator[](std::size_t i) const { return data_[i]; }
         void operator=(vector<T> v);
 };
 
@@ -61,5 +78,54 @@ void vector<T>::operator=(vector<T> v) {
     delete[] data_;
     data_ = new T[max_size_];
     std::copy(v.begin(), v.end(), data_);
+}
+
+template <typename T>
+void vector<T>::resize(std::size_t n){
+    if(n == max_size_) return;
+    T* tmp = new T[n];
+    if(size_ > n){
+        std::copy(data_, data_ + n, tmp);
+        size_ = n;
+    }
+    else
+        std::copy(data_, data_ + size_, tmp);
+    delete[] data_;
+    max_size_ = n; 
+    data_ = tmp;
+}
+
+template <typename T>
+void vector<T>::shrink_to_fit(){
+    if(size_ == max_size_) return;
+    T* tmp = new T[size_];
+    std::copy(data_, data_ + size_, tmp);
+    delete[] data_;
+    data_ = tmp;
+    max_size_ = size_;
+}
+
+template <typename T>
+void vector<T>::reserve(std::size_t n){
+    if(n > max_size_)
+        resize(n);
+}
+
+template <typename T>
+template <typename iter>
+void vector<T>::assign(iter first, iter last){
+    size_t n = last - first;
+    resize(n);
+    for(std::size_t i = 0; i < n; i++)
+        data_[i] = *(first++);
+    size_ = n;
+}
+
+template <typename T>
+void vector<T>::assign(std::size_t n, const T& v){
+    resize(n);
+    for(std::size_t i = 0; i < n; i++)
+        data_[i] = v;
+    size_ = n;
 }
 #endif // include guard
