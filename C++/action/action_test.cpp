fd78eaa92d580c214fc2dcdc527041ece9879b0a
Quinn Perfetto <thequinnftw@gmail.com>
2015-06-15 21:03:03 -0400

    updated test

--- C++/action/action_test.cpp
+++ C++/action/action_test.cpp
@@ -1,17 +1,33 @@
 #include "action.h"
 #include <iostream>
 
-void sq (int x) {
-    std::cout << x * x << std::endl;
+class Doer {
+public:
+    qap::action<int> on_even;
+    qap::action<int> on_odd;
+
+    void read() {
+        int n;
+        while (std::cin >> n) {
+            if (n % 2 == 0)
+                on_even(n);
+            else
+                on_odd(n);
+        }
+    }
+};
+
+void f(int x) {
+    std::cout << x << " is even" << std::endl;
 }
 
-void cube (int x) {
-    std::cout << x * x * x << std::endl;
+void g(int x) {
+    std::cout << x << " is odd" << std::endl;
 }
 
 int main() {
-    qap::action<int> f;
-    f += sq;
-    f += cube;
-    f(4);
+    Doer d;
+    d.on_even = f;
+    d.on_odd = g;
+    d.read();
 }
